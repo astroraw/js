fa5bfa604af324d6a98408fc72c58d81296ff265
astroRAW <52722486+astroraw@users.noreply.github.com>
2025-09-13 00:05:38 -0300

    Update tabbed-toc.js

--- tabbed-toc.js
+++ tabbed-toc.js
@@ -117,29 +117,29 @@
                 loading: 'Loading&hellip;',
                 noon: ['AM', 'PM'],
                 months: [
-                    'Enero',
-                    'Febrero',
-                    'Marzo',
-                    'Abril',
-                    'Mayo',
-                    'Junio',
-                    'Julio',
-                    'Agosto',
-                    'Septiembre',
-                    'Octubre',
-                    'Noviembre',
-                    'Diciembre'
+                    'January',
+                    'February',
+                    'March',
+                    'April',
+                    'May',
+                    'June',
+                    'July',
+                    'August',
+                    'September',
+                    'October',
+                    'November',
+                    'December'
                 ],
                 days: [
-                    'Domingo',
-                    'Lunes',
-                    'Martes',
-                    'Miércoles',
-                    'Jueves',
-                    'Viernes',
-                    'Sábado'
+                    'Sunday',
+                    'Monday',
+                    'Tuesday',
+                    'Wednesday',
+                    'Thursday',
+                    'Friday',
+                    'Saturday'
                 ],
-                recent: ' <sup>Nuevo!</sup>'
+                recent: ' <sup>New!</sup>'
             },
             query: {
                 'alt': 'json',
